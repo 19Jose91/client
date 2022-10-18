c64b98f500361854a508e32d31d25247c13b3b0f
19Jose91 <Jflores@icstars.org>
2022-10-18 01:01:42 -0500

    added a button with as a hyperilnk to future link

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,7 @@ const Home = () => {
                 </div>
                 <div className='relative w-16'>
                    <button className='relative bg-red-200 m-2 p-2 shadow-[0px_2px_4px_0px] top-[35px] left-[650px]'> <a href='/About'>About</a></button>
+                   <button className='relative bg-red-200 w-60 m-2 p-2 shadow-[0px_2px_4px_0px] top-[35px] left-[560px]'><a href='/Titles'>Enter Titles</a></button>
                 </div>
 
             </div>
