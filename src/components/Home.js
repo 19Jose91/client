5e37d99988b700903e877e30c20574ea2f38a950
19Jose91 <Jflores@icstars.org>
2022-10-18 00:16:47 -0500

    deleted some files that not part of the app

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <>
+            <div className='continer bg-red-300 mx-auto my-2 py-2'>
+                <div>
+                    <h1 className='text-[50px] text-center text-red-600' >Anime World</h1>
+                </div>
+                <div className='relative w-16'>
+                   <button className='relative bg-red-200 m-2 p-2 shadow-[0px_2px_4px_0px] top-[35px] left-[650px]'> <a href='/About'>About</a></button>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Home
