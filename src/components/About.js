acdd163b4569f0359b71a0f67268277f4d6cb076
19Jose91 <Jflores@icstars.org>
2022-10-18 22:40:04 -0500

    added some bg color and added a paragraph

--- src/components/About.js
+++ src/components/About.js
@@ -3,11 +3,12 @@ import React from 'react'
 const About = () => {
   return (
     <>
-    <div>
-        <div>
-            <h1>About</h1>
+      <div>
+        <div className='bg-sky-600 h-screen static'>
+          <h1 className='text-[60px] text-center text-white'>About</h1>
+          <p className='text-white text-[35px] text-center relative top-[40px]'>This site was created to help does looking for more anime titles</p>
         </div>
-    </div>
+      </div>
     </>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,13 @@ import React from 'react'
 const Home = () => {
     return (
         <>
-            <div className='continer bg-red-300 mx-auto my-2 py-2'>
+            <div className='continer bg-red-300 h-screen'>
                 <div>
-                    <h1 className='text-[50px] text-center text-red-600' >Anime World</h1>
+                    <h1 className='text-[60px] text-center text-red-600' >Anime World</h1>
                 </div>
                 <div className='relative w-16'>
-                   <button className='relative bg-red-200 m-2 p-2 shadow-[0px_2px_4px_0px] top-[35px] left-[650px]'> <a href='/About'>About</a></button>
-                   <button className='relative bg-red-200 w-60 m-2 p-2 shadow-[0px_2px_4px_0px] top-[35px] left-[560px]'><a href='/Titles'>Enter Titles</a></button>
+                   <button className='relative bg-red-200 m-2 p-2 shadow-[0px_2px_4px_0px] top-[35px] left-[650px] rounded-[20px]'> <a href='/About'>About</a></button>
+                   <button className='relative bg-red-200 w-60 m-2 p-2 shadow-[0px_2px_4px_0px] top-[35px] left-[560px] rounded-[20px]'><a href='/Titles'>Enter Titles</a></button>
                 </div>
 
             </div>
